0e53883290d0021859d01b25e46e71a628e67aa4
Mikolaj Bajtkiewicz <mikolaj.bajtkiewicz@tutanota.de>
2023-10-26 22:16:56 +0200

    Fixed config issue

--- flake.nix
+++ flake.nix
@@ -1,19 +1,15 @@
 {
   description = "Sk4rd's Wallpaper Flake";
 
-  outputs = { self, ... }:
-    let
-      pics = config.xdg.userDirs.pictures;
-    in
-      {
-        homeManagerModules.pictures = { config, lib, ... }: {
-          options.wallpapers.source = lib.mkOption {
-            type = lib.types.path;
-            default = "${self}/voxel";
-            description = "Wallpaper path";
-          };
-
-          config.home.file."${pics}/wallpapers".source = config.wallpapers.source;
-        };
+  outputs = { self, ... }: {
+    homeManagerModules.pictures = { config, lib, ... }: {
+      options.wallpapers.source = lib.mkOption {
+        type = lib.types.path;
+        default = "${self}/voxel";
+        description = "Wallpaper path";
       };
+
+      config.home.file."${config.xdg.userDirs.pictures}/wallpapers".source = config.wallpapers.source;
+    };
+  };
 }
