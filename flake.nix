f430f6bbcc7824979fafb0a20f8579f514c020f4
Mikolaj Bajtkiewicz <mikolaj.bajtkiewicz@tutanota.de>
2023-10-26 22:44:23 +0200

    Added ability to actually select wallpapers from repo

--- flake.nix
+++ flake.nix
@@ -3,13 +3,23 @@
 
   outputs = { self, ... }: {
     homeManagerModules.wallpapers = { config, lib, ... }: {
-      options.wallpapers.source = lib.mkOption {
-        type = lib.types.path;
-        default = "${self}/voxel";
-        description = "Wallpaper path";
+      options.wallpapers.selection = lib.mkOption {
+        type = lib.types.enum [ "voxel" "steampunk" "purin" "kuromi" ];
+        default = "voxel";
+        description = "Select the wallpaper directory theme";
       };
 
-      config.home.file."${config.xdg.userDirs.pictures}/wallpapers".source = config.wallpapers.source;
+      config.pictures.source = let
+        picMap = {
+          voxel = "${self}/voxel";
+          steampunk = "${self}/steampunk";
+          purin = "${self}/purin";
+          kuromi = "${self}/kuromi";
+        };
+      in
+        picMap.${config.wallpapers.selection};
+
+      home.file."${config.xdg.userDirs.pictures}/wallpapers" = config.wallpapers.source;
     };
   };
 }
