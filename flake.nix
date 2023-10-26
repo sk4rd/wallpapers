3ada3bf0cc802045d8e1211ba9df1acbe41d748d
Mikolaj Bajtkiewicz <mikolaj.bajtkiewicz@tutanota.de>
2023-10-26 23:18:59 +0200

    This is hell hopefully it workds

--- flake.nix
+++ flake.nix
@@ -2,26 +2,14 @@
   description = "Sk4rd's Wallpaper Flake";
 
   outputs = { self, ... }: {
-    homeManagerModules.wallpapers = { config, lib, ... }: {
-      options.wallpapers.selection = lib.mkOption {
-        type = lib.types.enum [ "voxel" "steampunk" "purin" "kuromi" ];
-        default = "voxel";
-        description = "Select the wallpaper directory theme";
+    homeManagerModules.wallpaper-module = { config, lib, ... }: {
+      options.wallpaper.theme = lib.mkOption {
+        type = lib.types.str;
+        description = "Wallpaper theme to use."
       };
 
-      config = {
-        wallpapers.source = let
-          picMap = {
-            voxel = "${self}/voxel";
-            steampunk = "${self}/steampunk";
-            purin = "${self}/purin";
-            kuromi = "${self}/kuromi";
-          };
-        in
-          picMap.${config.wallpapers.selection};
-        
-        home.file."${config.xdg.userDirs.pictures}/wallpapers" = config.wallpapers.source;
-      };
+      config.home.file."${config.xdg.userDirs.pictures}".source = "${self}/wallpapers/${config.wallpaper.theme}";
     };
+    
   };
 }
