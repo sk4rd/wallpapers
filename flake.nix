06164e54828d26913c62b45b1b55816cfa5ef2b5
Mikolaj Bajtkiewicz <mikolaj.bajtkiewicz@tutanota.de>
2023-10-26 23:21:52 +0200

    Forgot a semicolon

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     homeManagerModules.wallpaper-module = { config, lib, ... }: {
       options.wallpaper.theme = lib.mkOption {
         type = lib.types.str;
-        description = "Wallpaper theme to use."
+        description = "Wallpaper theme to use.";
       };
 
       config.home.file."${config.xdg.userDirs.pictures}".source = "${self}/wallpapers/${config.wallpaper.theme}";
