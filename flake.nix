45e49a171497701122ce1b5a75c9ee976edb976b
Mikolaj Bajtkiewicz <mikolaj.bajtkiewicz@tutanota.de>
2023-10-27 08:36:52 +0200

    Removed var shorthands

--- flake.nix
+++ flake.nix
@@ -3,24 +3,23 @@
 
   outputs = { self, ... }: {
     homeManagerModules.wallpaper-module = { config, lib, ... }:
-      let
-        opt = "options";
-        cfg = "config";
-        hf = "${cfg}.home.file";
-      in with lib; {
-        opt.wallpaper.theme = mkOption {
-          type = types.enum [ "voxel" "steampunk" "purin" "kuromi" ];
-          default = "voxel";
-          description = "Wallpaper theme to use.";
-        };
-        opt.wallpaper.directory = mkOption {
-          type = types.path;
-          default = "${cfg.xdg.userDirs.pictures}/wallpapers";
-          description = "Wallpaper directory path (must be in home directory).";
+      with lib; {
+        options = {
+          wallpaper.theme = mkOption {
+            type = types.enum [ "voxel" "steampunk" "purin" "kuromi" ];
+            default = "voxel";
+            description = "Wallpaper theme to use.";
+          };
+          wallpaper.directory = mkOption {
+            type = types.path;
+            default = "${cfg.xdg.userDirs.pictures}/wallpapers";
+            description =
+              "Wallpaper directory path (must be in home directory).";
+          };
         };
 
-        hf."${cfg.wallpaper.directory}".source =
-          "${self}/${cfg.wallpaper.theme}";
+        config.home.file."${config.wallpaper.directory}".source =
+          "${self}/${config.wallpaper.theme}";
       };
   };
 }
